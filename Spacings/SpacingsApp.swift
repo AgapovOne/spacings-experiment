4446d68f5d5dcb1dffb6ce0f5ea27d138091de4c
Alexey Agapov <agapov.one@gmail.com>
2022-07-04 17:27:25 +0500

    Initial Commit

---
+++ Spacings/SpacingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacingsApp.swift
+//  Spacings
+//
+//  Created by Alexey Agapov on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpacingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
